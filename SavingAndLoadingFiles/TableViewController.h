6960bca95a17cfdd39acad44837b0dbc108cca42
Nathan Cope <natecope@gmail.com>
2014-08-27 21:44:50 -0400

    Added gitignore

---
+++ SavingAndLoadingFiles/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  SavingAndLoadingFiles
+//
+//  Created by Nathan Cope on 8/24/14.
+//  Copyright (c) 2014 PlumChoice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
--- SavingAndLoadingFiles/ViewController.m
+++ SavingAndLoadingFiles/ViewController.m
@@ -8,11 +8,83 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *filenameTextField;
+@property (weak, nonatomic) IBOutlet UITextView *saveTextView;
+@property (weak, nonatomic) IBOutlet UITextView *loadTextView;
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSArray *files;
+}
+
+
+- (NSString *)documentsDirectory {
+    
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+}
+
+- (NSString *)pathInDocumentsDirectory:(NSString *)filename{
+
+    return [[self documentsDirectory] stringByAppendingPathComponent:filename];
+    
+}
+
+
+- (IBAction)saveButtonPressed:(id)sender {
+
+    [self.view endEditing:NO];
+    
+    // get the filename
+    NSString *filename = self.filenameTextField.text;
+    
+    // create path
+    NSString *path = [self pathInDocumentsDirectory:filename];
+    
+    // save data from textview
+    NSError *error = nil;
+    [self.saveTextView.text writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:&error];
+
+    if(error){
+        self.loadTextView.text = [error localizedDescription];
+    }
+}
+
+- (IBAction)loadButtonPressed:(id)sender {
+
+    [self.view endEditing:NO];
+    
+    //filepath to load from
+    NSString *filepath = [self pathInDocumentsDirectory:self.filenameTextField.text];
+    
+    
+    //load the string data to the text area
+    NSError *error = nil;
+    
+    NSString *loadedTextData = [NSString stringWithContentsOfFile:filepath encoding:NSUTF8StringEncoding error:&error];
+   
+    self.loadTextView.text = loadedTextData;
+    
+    if(error){
+        self.loadTextView.text = [error localizedDescription];
+    }
+    
+    
+    
+}
+
+- (NSArray *)getFileList{
+    NSArray *fileList = [[NSArray alloc]init];
+    
+    NSFileManager *filemanager = [[NSFileManager alloc]init];
+    
+    NSError *error = nil;
+    
+    fileList = [filemanager contentsOfDirectoryAtPath:[self documentsDirectory] error:&error];
+    
+    return fileList;
+}
 
 - (void)viewDidLoad
 {
@@ -46,7 +118,27 @@
     }
     
     NSLog(@"Loaded text data: %@", loadedTextData);
+    
+    files = [self getFileList];
 }
 
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+
+    
+    return [files count];
+    
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = [files objectAtIndex:indexPath.row];
+    
+    return cell;
+}
 
 @end
